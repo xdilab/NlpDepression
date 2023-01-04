218e27fbbeed1d0f9fa83867e4a7f755d00ff92f
dmlee1 <70760991+dmlee1@users.noreply.github.com>
2023-01-04 08:58:31 -0600

    Added new subsidiary files that spread out functions from original HelperFunctions to improve readability and finding functions

---
+++ ImportFunctions.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+from Libraries import *
+from HelperFunctions import getLabels, extractList
+
+def importUMD(UMD_path, anno_type, task_type=None):
+    chunksize = 10 ** 6
+
+    if anno_type == "crowd":
+        ## For Train
+
+        # Get all posts
+        tfr = pd.read_csv(os.path.join(UMD_path, anno_type, "train", "shared_task_posts.csv"), chunksize=chunksize,
+                          iterator=True)
+        all_train_posts = pd.concat(tfr, ignore_index=True)
+
+        # Get Labels
+        train_labels = pd.read_csv(os.path.join(UMD_path, anno_type, "train", "crowd_train.csv"))
+
+        # Get appropriate task posts; if none provided, ignore
+        if task_type.upper() in (["A", "B", "C"]):
+            train_task_nexus = pd.read_csv(
+                os.path.join(UMD_path, anno_type, "train", f"task_{task_type.upper()}_train.posts.csv"))
+
+        # Get all posts in task
+        task_train = all_train_posts[all_train_posts.post_id.isin(train_task_nexus.post_id)]
+        # Add Labels
+        task_train = pd.merge(task_train, train_labels, how="left", on="user_id")
+
+        ## For Test
+
+        # # Get all posts
+        tfr = pd.read_csv(os.path.join(UMD_path, anno_type, "test", "shared_task_posts_test.csv"), chunksize=chunksize,
+                          iterator=True)
+        all_test_posts = pd.concat(tfr, ignore_index=True)
+
+        # Get Labels
+        test_labels = pd.read_csv(os.path.join(UMD_path, anno_type, "test", "crowd_test.csv"))
+
+        # Get appropriate task posts; if none provided, ignore
+        if task_type.upper() in (["A", "B", "C"]):
+            test_task_nexus = pd.read_csv(
+                os.path.join(UMD_path, anno_type, "test", f"task_{task_type.upper()}_test.posts.csv"))
+
+        # Get all posts in task
+        task_test = all_test_posts[all_test_posts.post_id.isin(test_task_nexus.post_id)]
+        # Add Labels
+        task_test = pd.merge(task_test, test_labels, how="left", on="user_id")
+
+        return task_train, task_test
+
+    elif anno_type == "expert":
+        # Get all posts
+        tfr = pd.read_csv(os.path.join(UMD_path, anno_type, "expert_posts.csv"), chunksize=chunksize,
+                          iterator=True)
+        expert_posts = pd.concat(tfr, ignore_index=True)
+        # Get Labels
+        expert_labels = pd.read_csv(os.path.join(UMD_path, anno_type, "expert.csv"))
+        # Add Labels
+        all_expert_posts = pd.merge(expert_posts, expert_labels, how="left", on="user_id")
+
+        return all_expert_posts
+
+
+def importCSSRS(filepath, num_labels=4):
+    CSSRS = pd.read_csv(filepath)
+    CSSRS, inv_map = getLabels(CSSRS, num_labels)
+    # Extract List from string
+    extractList(CSSRS)
+    # Concatenate Posts into one long string
+    CSSRS["Post"] = CSSRS["Post"].apply(lambda x: " ".join(x))
+    return CSSRS
+
+
+def getModel(modelType):
+    if modelType.upper() == "BERT":
+        model_name = 'bert-base-uncased'
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = BertForMaskedLM.from_pretrained(model_name)
+    elif modelType.upper() == "ROBERTA":
+        model_name = 'roberta-base'
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = RobertaForMaskedLM.from_pretrained(model_name)
+    elif modelType.upper() == "ELECTRA":
+        model_name = 'google/electra-base-discriminator'
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = ElectraForMaskedLM.from_pretrained(model_name)
+
+    return tokenizer, model
+
+def getRegularModel(modelType):
+    if modelType.upper() == "BERT":
+        model_name = 'bert-base-uncased'
+        model = BertModel.from_pretrained(model_name)
+    elif modelType.upper() == "ROBERTA":
+        model_name = 'roberta-base'
+        model = RobertaModel.from_pretrained(model_name)
+    elif modelType.upper() == "ELECTRA":
+        model_name = 'google/electra-base-discriminator'
+        model = ElectraModel.from_pretrained(model_name)
+
+    return model
+
+def getTokenizer(modelType):
+    if modelType.upper() == "BERT":
+        model_name = 'bert-base-uncased'
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+    elif modelType.upper() == "ROBERTA":
+        model_name = 'roberta-base'
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+    elif modelType.upper() == "ELECTRA":
+        model_name = 'google/electra-base-discriminator'
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    return tokenizer
---
+++ StatisticsFunctions.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+from Libraries import *
+from HelperFunctions import mergeDicts
+
+def multiclass_accuracy(matrix, num_classes):
+    return {str(i): {"acc": (matrix.diagonal() / matrix.sum(axis=1)).tolist()[i]} for i in range(num_classes)}
+
+def multiclass_ROC_AUC(y_test, y_pred_proba, n_classes):
+    fpr = []
+    tpr = []
+    roc_auc = {}
+    for i in range(n_classes):
+        df_aux = pd.DataFrame()
+        df_aux['class'] = [1 if y == i else 0 for y in y_test]
+        df_aux['prob'] = y_pred_proba[:, i]
+        df_aux = df_aux.reset_index(drop=True)
+        fpr_i, tpr_i, _ = roc_curve(df_aux['class'], df_aux['prob'])
+        fpr.append(fpr_i)
+        tpr.append(tpr_i)
+        aoc_val = auc(fpr[i], tpr[i])
+        roc_auc[str(i)] = {"auc": aoc_val}
+    return(roc_auc)
+
+def getStatistics(outputPath, y_test, y_pred_proba, y_pred, n_lab):
+    y_pred_proba = np.array(y_pred_proba.tolist())
+
+    # Multi-label
+    print(classification_report(y_test, y_pred))
+    # contains precision, recall, and f1 score for each class
+    report = classification_report(y_test, y_pred, output_dict=True)
+
+    # Get only precision, recall, f1-score, and support statistics
+    # filtered_report = {str(label): report[str(label)] for label in range(y_test.nunique())}
+
+    matrix = confusion_matrix(y_test, y_pred)
+    print(f"{n_lab}-label confusion matrix")
+    print(matrix)
+
+    label_accuracies = multiclass_accuracy(matrix, n_lab)
+    class_auc = multiclass_ROC_AUC(y_test, y_pred_proba, n_lab)
+    aucValue = roc_auc_score(y_test, y_pred_proba, multi_class="ovr", average='macro')
+
+    new_fp = (sum(y_pred > y_test.to_numpy())) / len(y_pred)
+    new_fn = (sum(y_pred < y_test.to_numpy())) / len(y_pred)
+    oe = (sum(abs(y_pred - y_test.to_numpy()) > 1)) / len(y_pred)
+    print(f"New False Positive Rate = {new_fp}")
+    print(f"New False Negative Rate = {new_fn}")
+    print(f"New Ordinal Error = {oe}")
+
+    # argument is list of dictionaries to merge
+    class_statistics = mergeDicts([report, label_accuracies, class_auc])
+    class_statistics["matrix"] = matrix
+    class_statistics["auc"] = aucValue
+    class_statistics["graded_fp"] = new_fp
+    class_statistics["graded_fn"] = new_fn
+    class_statistics["ordinal_err"] = oe
+
+    return class_statistics
+
+def getSummStats(CSSRS):
+    # return
+    #Raw number of each label in 5-label schema
+    print("Frequency of each label in 5-label schema")
+    print(CSSRS.value_counts("Label"))
+
+    #Percentage of each label in 5-label schema
+    print("Percentage of each label in 5-label schema")
+    CSSRS["Label"].value_counts(normalize=True) * 100
+
+
+    #Convert from 5-label to 4-label
+    scale_conversion4 = {"Supportive": "No-risk",
+                         "Indicator": "No-risk",
+                         "Ideation": "Ideation",
+                         "Behavior": "Behavior",
+                         "Attempt": "Attempt"}
+
+    CSSRS_4 = CSSRS.replace({"Label":scale_conversion4})
+
+    #Raw number of each label in 4-label schema
+    print("Frequency of each label in 4-label schema")
+    print(CSSRS_4.value_counts("Label"))
+
+    #Percentage of each label in 4-label schema
+    print("Percentage of each label in 4-label schema")
+    print(CSSRS_4["Label"].value_counts(normalize=True) * 100)
+
+def getTokenStats(df, len_of_interest = 512):
+    token_lengths = []
+    for i in range(len(df["model_input"])):
+        token_lengths.append(len(df["model_input"][i]["input_ids"][0]))
+    print(f"Max Length: {max(token_lengths)}")
+    print(f"Min Length: {min(token_lengths)}")
+    print(f"Mean Length: {mean(token_lengths)}")
+    print(f"Median Length: {median(token_lengths)}")
+
+    num_under = 0
+    for tok_length in token_lengths:
+        if tok_length <= 512:
+            num_under += 1
+    print(f"Percent under or exactly {len_of_interest} tokens: {num_under/len(token_lengths)}")
